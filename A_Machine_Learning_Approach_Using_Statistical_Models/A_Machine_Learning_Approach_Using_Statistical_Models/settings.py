02f0de17c17487bd1923f10c9b65212cc143973f
ajay501-python <ajayaddanki55@gmail.com>
2025-07-21 10:46:37 +0530

    Update settings.py

--- A_Machine_Learning_Approach_Using_Statistical_Models/A_Machine_Learning_Approach_Using_Statistical_Models/settings.py
+++ A_Machine_Learning_Approach_Using_Statistical_Models/A_Machine_Learning_Approach_Using_Statistical_Models/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
